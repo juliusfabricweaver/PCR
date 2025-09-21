85c8231f4a06ed226cc6763a2b7592ca8fffa288
Julien Hayek <julien@tomorrow.services>
2025-09-21 22:55:31 +0300

    dev tools prod

--- electron/main.js
+++ electron/main.js
@@ -31,6 +31,8 @@ function createWindow() {
     mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, '..', 'dist', 'frontend', 'index.html'));
+    // Temporarily enable DevTools in production for debugging
+    mainWindow.webContents.openDevTools();
   }
 
   mainWindow.on('closed', () => {
